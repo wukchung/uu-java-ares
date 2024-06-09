85dd531a13fdac68d9d724679170bc3b7b3b2163
Daniel Král <kraldaniel.cze@gmail.com>
2024-06-09 16:08:24 +0200

    CRUD for ares entity

--- ares-proxy/src/main/java/com/workshop/ares_proxy/model/AresEntity.java
+++ ares-proxy/src/main/java/com/workshop/ares_proxy/model/AresEntity.java
@@ -1,5 +1,6 @@
 package com.workshop.ares_proxy.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,7 +14,8 @@ public class AresEntity {
     private Long id;
     
     private String ico;
-
+    
+    @Column(columnDefinition = "TEXT")
     private String json_data;
 
     public AresEntity() {
--- ares-proxy/src/main/java/com/workshop/ares_proxy/repository/AresEntityRepository.java
+++ ares-proxy/src/main/java/com/workshop/ares_proxy/repository/AresEntityRepository.java
@@ -2,9 +2,13 @@
 package com.workshop.ares_proxy.repository;
 
 import com.workshop.ares_proxy.model.AresEntity;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AresEntityRepository extends JpaRepository<AresEntity, String> {
+	Optional<AresEntity> findByIco(String ico);
 }
